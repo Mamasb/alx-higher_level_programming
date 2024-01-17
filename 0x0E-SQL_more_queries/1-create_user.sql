4ed2db01fe0c1eff615d5942d63626356305dea9
carheywho <pkpipimwangi@gmail.com>
2024-01-17 13:39:12 -0800

    This script  creates the MYSQL server user_0d_1. The usrer should have priviledges on the MYSQL server. User password is set to user_0d_1pwd.The script should fail if the useralready exists

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create user 'user_0d_1' with all priviledges
+-- Password for user set as 'user_0d_1_pwd'
+-- If user already exists, script should fail
+CREATE UHSER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON *.*
+TO 'user_0d_1'@'locolhost';
+FLUSH PRIVILEDGES
