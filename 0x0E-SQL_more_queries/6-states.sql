7ad2d41421e171a1cde7997dd2b4fc23908225e0
carheywho <pkpipimwangi@gmail.com>
2024-01-17 14:30:34 -0800

    A scripr that creates database hbtn_9d_usa and the table states ( in thwe database hntn_0d_usa) on MYSQL server. id INT unique, auto generated, cam't be null. i database exists your script should not fail same as the table.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- if table arleady exists, script should not fail.
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL.
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
