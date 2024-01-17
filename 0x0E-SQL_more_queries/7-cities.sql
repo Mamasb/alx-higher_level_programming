b621562a13ffa8c9655b0c2c4cf9cd3a93146312
carheywho <pkpipimwangi@gmail.com>
2024-01-17 14:51:43 -0800

    a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
    cities description:
    id INT unique, auto generated, can’t be null and is a primary key
    state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
    name VARCHAR(256) can’t be null
    If the database hbtn_0d_usa already exists, your script should not fail
    If the table cities already exists, your script should not fail

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,12 @@
 -- Create db 'hbtn_0d_usa'
--- If db aready exists, script should not fail
+-- If db already exists, script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
---create tables 'cities' in db 'hbtn_0d_usa'
--- id INT uniquem, auto-generated, not null, primary key
--- state_id INT not null, foreign key that refrences if=d of 'ststes' table
+-- Create table 'cities' in db 'hbtn_0d_usa'
+-- id INT unique, auto generated, not null, primary key
+-- state_id INT not null, foreign key that references id of 'states' table
 -- name VARCHAR(256) not null
---  If table already exists, script should not fail
-
+-- If table already exists, script should not fail
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
@@ -15,5 +14,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id)
-		REFRENCES hbtn_0d_usa.states(id)
+		REFERENCES hbtn_0d_usa.states(id)
 );
