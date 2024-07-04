a21f954cfab21398efa0a60bb3a7dc487073e4ac
mamasb <pkpipimwangi@gmail.com>
2024-07-04 23:31:39 +0300

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -so /dev/null "$1" -w '%{size_download}\n'#!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -so /dev/null "$1" -w '%{size_download}\n'
+# Bash script
