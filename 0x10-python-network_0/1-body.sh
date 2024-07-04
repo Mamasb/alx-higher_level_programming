6bb54689ab21091d6121887f55785d54c995a3ff
mamasb <pkpipimwangi@gmail.com>
2024-07-05 00:10:12 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# takes in a URL sends a GET request to URL and displays body
 curl -sLf $1
