4626d2575296668e5d13e89aaf04fa2a3aa82d4a
mamasb <pkpipimwangi@gmail.com>
2024-07-31 13:08:46 +0300

    modified

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-let fs = require('fs');
-let file = process.argv[2];
-
-fs.readFile(file, 'utf8', function (err, data) {
-  if (err) console.log(err);
-  else console.log(data);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
